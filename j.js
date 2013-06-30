ea541e486f013a67e8d572f520fb36c277312ed6
Chris Edgemon <chrisedgemon@gmail.com>
2013-06-29 23:17:33 -0400

    Introduce coquette framework.

--- j.js
+++ j.js
@@ -4,7 +4,43 @@
     
     var loaded = false;
 
-    var gameCanvas, gameContext;
+
+    var Game = function(canvasId, width, height) {
+      var coq = new Coquette(this, canvasId, width, height, "#000");
+
+      coq.entities.create(Person, { pos:{ x:843, y:40 }, color:"#099" }); // paramour
+      coq.entities.create(Person, 
+                          { pos:{ x:849, y:210 }, color:"#f07", // player
+                            update: function() {
+                              var SPEED = 2.4;
+
+                              var directions = {
+                              }
+
+                              for (key in directions){
+                                if (coq.inputter.state(key)){
+                                  var d = directions[key];
+                                  this.pos.x += d[0]
+                                  this.pos.y += d[1]
+                                }
+                              }
+                            },
+                            collision: function(other) {
+                              other.pos.y = this.pos.y; // follow the player
+                            }
+                          });
+    };
+
+    var Person = function(_, settings) {
+      for (var i in settings) {
+        this[i] = settings[i];
+      }
+      this.size = { x:20, y:20 };
+      this.draw = function(ctx) {
+        ctx.fillStyle = settings.color;
+        ctx.fillRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+      };
+    };
 
 
     $("#game").click(function(){
@@ -13,13 +49,7 @@
         var t = $(this)
         t.html('');
         t.append($('<canvas/>', {'id': 'gameCanvas','Width': t.width(), 'Height': t.height()}));
-        gameCanvas = document.getElementById("gameCanvas");
-        gameContext = gameCanvas.getContext('2d');
-
-        $("#gameCanvas").click(function(){
-          alert("playing!");
-        })
-
+        new Game("gameCanvas", t.width(), t.height());
       }
     });
 
