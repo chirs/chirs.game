d6c5011d35906520f66dcd0cc0a0113cad52e7db
Chris Edgemon <chrisedgemon@gmail.com>
2017-03-15 20:06:33 +0000

    clean out unnecessary code in pacman.js; move old pacman code to reflect.js

--- src/reflect.js
+++ src/reflect.js
@@ -58,6 +58,21 @@
 	    this.state = this.STATE.PLAY
 
 
+	    for (var i=0; i < this.level; i++){
+		//var x = Math.random() * width;
+		//var y = Math.random() * height;
+		var x = width / 2;
+		var y = height / 2;
+		this.coquette.entities.create(Ball, { pos:{ x:x, y:y }}); // The Ball
+	    };
+
+	    var walls = [
+		[25, 25, 300, 'x'],
+		[25, 25, 800, 'y'],
+		[85, 25, 300, 'x'],
+		[25, 325, 800, 'y']
+	    ]
+
 
 	    this.coquette.entities.create(Wall, {
 		game: self,
@@ -136,7 +151,9 @@
 		
 		
 		collision: function(other) {
-
+		    if (other instanceof Ball){
+			other.vel.x = -1 * other.vel.x;
+		    }
 		    if (other instanceof Wall){
 			this.direction = this.OPPOSITES[this.direction];
 			this.dir = this.directions[this.direction];
@@ -144,6 +161,42 @@
 		    
 		}
 	    });
+
+	    /*
+	    this.coquette.entities.create(Opponent, { 
+		game: self,
+		pos:{ x:549, y:110 },
+		vel:{ x: 0, y: 1 },
+		color:"#0ff", // light blue
+		
+		SHOOT_DELAY: 300,
+		lastShot: 0,
+		
+		
+		update: function(tick) {
+		    var speed = 2;
+
+		    var my = this.vel.y * tick;
+		    this.pos.y += my;
+		    this.vel.y += .01 * Math.random() * Math.random() * plusMinus();
+		
+		    if (!this.game.coquette.renderer.onScreen(this)) {
+			this.vel.y = -1 * this.vel.y	    		    
+
+		    }
+		},
+		
+		
+		collision: function(other) {
+		    if (other instanceof Ball){
+			console.log('help');
+			this.score += 1;
+			Game.score += 1;
+			other.vel.x = -1 * other.vel.x;
+		    }
+		}
+	    });	    
+	    */
 	
 	};
 	
@@ -192,6 +245,31 @@
 	Game.prototype =  {
 	    draw: function(ctx) {
 		
+		
+		if (this.coquette.entities.all(Ball).length == 0){
+		    this.level += 1;
+		    this.state = this.STATE.WIN;
+
+		    console.log('you win');
+
+		    $("#next").show();
+
+		    /*
+		    ctx.fillStyle = "#aaa"
+		    ctx.fillRect(0, 0, 400, 400);
+		    ctx.lineWidth=1;
+		    ctx.fillStyle = "#fff"
+		    
+		    ctx.font = "44px sans-serif";
+		    ctx.fillText("you win", 100, 100);
+		    
+		    ctx.font = "22px sans-serif";          
+		    ctx.fillText("next level", 100, 140);
+		    */
+
+
+		    
+		}
 		
 		if (this.state === this.STATE.LOSE){
 		    ctx.fillStyle = "#ccc"
@@ -249,6 +327,108 @@
 
 	// Wall	
 	
+	var Ball = function(game, settings){
+	    this.game = game
+	    // shame
+	    for (var i in settings) {
+		this[i] = settings[i];
+	    }
+	    this.size = { x:9, y:9 };
+
+	    this.vel = {x: 20 * makeVel(), y: 5 * makeVel()} // This is just a vector?
+	};
+	
+	
+	
+	Ball.prototype = {
+	    
+	    draw: function(ctx) {
+		ctx.fillStyle = this.color();
+		ctx.fillRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+	    },
+
+	    collision: function(other) {
+		if (other instanceof Wall){
+		    if (other.direction == 'x'){
+			this.vel.x = -1 * this.vel.x;
+		    } else {
+			this.vel.y = -1 * this.vel.y;
+		    }
+		}
+	    },
+	    
+	    color: function(){
+		if (this.shielded){
+		    return "#0f0";
+		} else {
+		    return "#fff";
+		}
+	    },
+	    
+	    kill: function() {
+		this.game.coquette.entities.destroy(this);
+	    },
+	    
+	    update: function(tick) {
+		
+		var mx = this.vel.x * tick;
+		var my = this.vel.y * tick;
+		this.pos.x += mx;
+		this.pos.y += my;
+		
+		if (!this.game.coquette.renderer.onScreen(this)) {
+		    this.game.state = this.game.STATE.LOSE;
+		}
+		
+	    }
+	}
+	
+	// Bullet.
+	
+	var Bullet = function(game, settings) {
+	    this.game = game;
+	    this.pos = settings.pos;
+	    this.vel = settings.vector;
+	};
+	
+	Bullet.prototype = {
+            size: { x:12, y:12 },
+            speed: .2,
+	    
+            update: function(tick) {
+		
+		var mx = this.vel.x * tick * this.speed;
+		var my = this.vel.y * tick * this.speed;
+		this.pos.x += mx;
+		this.pos.y += my;
+		
+		if (!this.game.coquette.renderer.onScreen(this)) {
+		    this.kill();
+		}
+            },
+	    
+	    
+            draw: function(ctx) {
+		ctx.fillStyle = "#888";
+		ctx.fillRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+            },
+	    
+            collision: function(other) {
+		if (other instanceof Ball) {
+		    this.kill();
+		    if (other.shielded === false){
+			if (this.game.state === this.game.STATE.PLAY){
+			    this.game.score += 1;
+			}
+			other.kill();
+		    }
+		}
+            },
+	    
+            kill: function() {
+		this.game.coquette.entities.destroy(this);
+            }
+	}
 
 	var startGame = function(){
 	    if (loaded === false){
