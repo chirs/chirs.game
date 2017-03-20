05a873ff6dd964e0a2c8a2c1c36a0030c9e0b227
Chris Edgemon <chrisedgemon@gmail.com>
2017-03-20 19:18:47 +0000

    clean out pong.js

--- src/pong.js
+++ src/pong.js
@@ -4,42 +4,7 @@
 
 	var loaded = false; // Game has been loaded?
 	
-	// 50/50 [coin flip]
-	var plusMinus = function(){
-	    if (Math.random() < .5){
-		return -1;
-	    }
-	    return 1;
-	}
-
-	
 	var timePassed = function(last, interval) { return last + interval < new Date().getTime(); };
-	
-	// find the center of a rectangle
-	var center = function(obj) {
-	    if(obj.pos !== undefined) {
-		return {
-		    x: obj.pos.x + (obj.size.x / 2),
-		    y: obj.pos.y + (obj.size.y / 2),
-		};
-	    }
-	};
-	
-	// Find all entities that are colliding.
-	var getCollidingEntities = function(collisions, entity){
-	    var entities = []
-	    for (var i=0; i < collisions.length; i++){
-		var colliding = collisions[i];
-		if (colliding[0] === entity){
-		    entities.push(colliding[1]);
-		};
-		if (colliding[1] === entity){
-		    entities.push(colliding[0]);
-		};
-	    }
-	    return entities;
-	};
-	
 	
 	var Game = function(canvasId, width, height) {
 	    
@@ -66,148 +31,19 @@
 		this.coquette.entities.create(Ball, { pos:{ x:x, y:y }}); // The Ball
 	    };
 
-	    var walls = [
-		[25, 25, 300, 'x'],
-		[25, 25, 800, 'y'],
-		[85, 25, 300, 'x'],
-		[25, 325, 800, 'y']
-	    ]
-
-
-	    this.coquette.entities.create(Wall, {
-		game: self,
-		pos: {x: 25, y: 25 },
-		length: 100,
-		direction: 'x'
-	    })
-
-	    this.coquette.entities.create(Wall, {
-		game: self,
-		pos: {x: 25, y: 225 },
-		length: 100,
-		direction: 'x'
-	    })	    
-
-	    this.coquette.entities.create(Wall, {
-		game: self,
-		pos: {x: 625, y: 25 },
-		length: 100,
-		direction: 'x'
-	    })
-
-	    this.coquette.entities.create(Wall, {
-		game: self,
-		pos: {x: 625, y: 225 },
-		length: 100,
-		direction: 'x'
-	    })	    	    
-
-	    this.coquette.entities.create(Wall, {
-		game: self,
-		pos: {x: 25, y: 25 },
-		length: 600,
-		direction: 'y'
-	    })
-
-	    this.coquette.entities.create(Wall, {
-		game: self,
-		pos: {x: 25, y: 325 },
-		length: 600,
-		direction: 'y'
-	    })	    
-
+	    this.createWalls();
 
 	    
 	    this.coquette.entities.create(Person, { 
 		game: self,
-		pos:{ x:249, y:110 }, 
+		pos:{ x:200, y:100 }, 
 		color:"#0ff", // light blue
 		
 		SHOOT_DELAY: 300,
 		lastShot: 0,
-		
-		
-		update: function() {
-		    var speed = 2;
-		    var directions = {
-			'UP_ARROW': [0, -speed],
-			'DOWN_ARROW': [0, speed],
-		    } 
-		    
-		    
-		    for (key in directions){
-			if (self.coquette.inputter.state(self.coquette.inputter[key])){
-			    var dir = directions[key]
-			    this.pos.x += dir[0];
-			    this.pos.y += dir[1];
-			}
-		    }
-
-		},
-		
-		
-		collision: function(other) {
-		    if (other instanceof Ball){
-			console.log('help');
-			this.score += 1;
-			Game.score += 1;
-			other.vel.x = -1 * other.vel.x;
-		    }
-		}
 	    });
-
-	    /*
-	    this.coquette.entities.create(Opponent, { 
-		game: self,
-		pos:{ x:549, y:110 },
-		vel:{ x: 0, y: 1 },
-		color:"#0ff", // light blue
-		
-		SHOOT_DELAY: 300,
-		lastShot: 0,
-		
-		
-		update: function(tick) {
-		    var speed = 2;
-
-		    var my = this.vel.y * tick;
-		    this.pos.y += my;
-		    this.vel.y += .01 * Math.random() * Math.random() * plusMinus();
-		
-		    if (!this.game.coquette.renderer.onScreen(this)) {
-			this.vel.y = -1 * this.vel.y	    		    
-
-		    }
-		},
-		
-		
-		collision: function(other) {
-		    if (other instanceof Ball){
-			console.log('help');
-			this.score += 1;
-			Game.score += 1;
-			other.vel.x = -1 * other.vel.x;
-		    }
-		}
-	    });	    
-	    */
-	
 	};
-	
-	var Box = function(game, settings){
-	    this.size = settings.size || {height:9, width:9 };
-	    this.position = settings.position || {x:0, y:0 };
-	    this.color = setting.scolor || "#ccc";
 	    
-	    this.draw = function(ctx) {
-		ctx.fillStyle = "#fff"
-		ctx.fillRect(this.position.x0, this.position.y, this.size.height, this.size.width);
-		ctx.lineWidth=1;
-		ctx.fillStyle = "#ccc"
-		ctx.font = "44px sans-serif";
-	    }
-	    
-	};
 	
 	var Person = function(game, settings) {
 	    for (var i in settings) {
@@ -219,6 +55,37 @@
 		ctx.fillRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
 	    };
 	};
+
+
+	Person.prototype.update = function() {
+	    var speed = 2;
+	    var directions = {
+		'UP_ARROW': [0, -speed],
+		'DOWN_ARROW': [0, speed],
+	    } 
+	    
+	    
+	    for (key in directions){
+		if (this.game.coquette.inputter.state(this.game.coquette.inputter[key])){
+		    var dir = directions[key]
+		    this.pos.x += dir[0];
+		    this.pos.y += dir[1];
+		}
+	    }
+	    
+	};
+		
+		
+	Person.prototype.collision = function(other) {
+	    if (other instanceof Ball){
+		console.log('help');
+		this.score += 1;
+		Game.score += 1;
+		other.vel.x = -1 * other.vel.x;
+	    }
+	};
+	
+	
 
 	var Opponent = function(game, settings) {
 	    for (var i in settings) {
@@ -236,56 +103,96 @@
 	}
 	
 	
-	Game.prototype =  {
-	    draw: function(ctx) {
-		
-		
-		if (this.coquette.entities.all(Ball).length == 0){
-		    this.level += 1;
-		    this.state = this.STATE.WIN;
-
-		    console.log('you win');
-
-		    $("#next").show();
-
-		    /*
-		    ctx.fillStyle = "#aaa"
-		    ctx.fillRect(0, 0, 400, 400);
-		    ctx.lineWidth=1;
-		    ctx.fillStyle = "#fff"
-		    
-		    ctx.font = "44px sans-serif";
-		    ctx.fillText("you win", 100, 100);
-		    
-		    ctx.font = "22px sans-serif";          
-		    ctx.fillText("next level", 100, 140);
-		    */
-
-
-		    
-		}
-		
-		if (this.state === this.STATE.LOSE){
-		    ctx.fillStyle = "#ccc"
-		    ctx.fillRect(0, 0, 1020, 1020);
-		    ctx.lineWidth=1;
-		    ctx.fillStyle = "#666"
-		    
-		    ctx.font = "44px sans-serif";
-		    ctx.fillText("game over", 400, 100);
-		    
-		    ctx.font = "22px sans-serif";          
-		    ctx.fillText("play again", 400, 140);
-		};
-		
-		ctx.lineWidth=1;
-		ctx.fillStyle = "#fff";
-		ctx.font = "18px sans-serif";
-		ctx.fillText("Score: " + this.score, 20, 20);
-		
+	Game.prototype.draw = function(ctx) {
+	    if (this.coquette.entities.all(Ball).length == 0){
+		this.level += 1;
+		this.state = this.STATE.WIN;
+		console.log('you win');
+		$("#next").show();
 	    }
+		
+	    if (this.state === this.STATE.LOSE){
+		ctx.fillStyle = "#ccc"
+		ctx.fillRect(0, 0, 1020, 1020);
+		ctx.lineWidth=1;
+		ctx.fillStyle = "#666"
+		
+		ctx.font = "44px sans-serif";
+		ctx.fillText("game over", 400, 100);
+		
+		ctx.font = "22px sans-serif";          
+		ctx.fillText("play again", 400, 140);
+	    };
+	    
+	    ctx.lineWidth=1;
+	    ctx.fillStyle = "#fff";
+	    ctx.font = "18px sans-serif";
+	    ctx.fillText("Score: " + this.score, 20, 20);
 	    
 	};
+
+	Game.prototype.createWalls = function(){
+
+	    
+	    this.coquette.entities.create(Wall, {
+		game: self,
+		pos: {x: 25, y: 25 },
+		length: 100,
+		direction: 'x'
+	    })
+	    
+	    this.coquette.entities.create(Wall, {
+		game: self,
+		pos: {x: 25, y: 225 },
+		length: 100,
+		direction: 'x'
+	    })	    
+	    
+	    this.coquette.entities.create(Wall, {
+		game: self,
+		pos: {x: 625, y: 25 },
+		length: 100,
+		direction: 'x'
+	    })
+	    
+	    this.coquette.entities.create(Wall, {
+		game: self,
+		pos: {x: 625, y: 150 },
+		length: 200,
+		direction: 'x'
+	    })
+
+
+	    this.coquette.entities.create(Wall, {
+		game: self,
+		pos: {x: 825, y: 25 },
+		length: 300,
+		direction: 'x'
+	    })
+	    
+	    this.coquette.entities.create(Wall, {
+		game: self,
+		pos: {x: 825, y: 350 },
+		length: 100,
+		direction: 'x'
+	    })	    	  	    
+	    
+	    this.coquette.entities.create(Wall, {
+		game: self,
+		pos: {x: 25, y: 25 },
+		length: 1200,
+		direction: 'y'
+	    })
+	    
+	    this.coquette.entities.create(Wall, {
+		game: self,
+		pos: {x: 25, y: 325 },
+		length: 1200,
+		direction: 'y'
+	    })	    
+	};
+
+	
 	
 	// Wall
 
@@ -334,14 +241,12 @@
 	
 	
 	
-	Ball.prototype = {
-	    
-	    draw: function(ctx) {
+	Ball.prototype.draw = function(ctx){
 		ctx.fillStyle = this.color();
 		ctx.fillRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
-	    },
+	};
 
-	    collision: function(other) {
+	Ball.prototype.collision = function(other) {
 		if (other instanceof Wall){
 		    if (other.direction == 'x'){
 			this.vel.x = -1 * this.vel.x;
@@ -349,33 +254,33 @@
 			this.vel.y = -1 * this.vel.y;
 		    }
 		}
-	    },
+	};
 	    
-	    color: function(){
+	Ball.prototype.color = function(){
 		if (this.shielded){
 		    return "#0f0";
 		} else {
 		    return "#fff";
 		}
-	    },
+	};
 	    
-	    kill: function() {
+	Ball.prototype.kill = function() {
 		this.game.coquette.entities.destroy(this);
-	    },
+	};
 	    
-	    update: function(tick) {
+	Ball.prototype.update = function(tick) {
 		
-		var mx = this.vel.x * tick;
-		var my = this.vel.y * tick;
-		this.pos.x += mx;
-		this.pos.y += my;
-		
-		if (!this.game.coquette.renderer.onScreen(this)) {
-		    this.game.state = this.game.STATE.LOSE;
-		}
-		
+	    var mx = this.vel.x * tick;
+	    var my = this.vel.y * tick;
+	    this.pos.x += mx;
+	    this.pos.y += my;
+	    
+	    if (!this.game.coquette.renderer.onScreen(this)) {
+		this.game.state = this.game.STATE.LOSE;
 	    }
-	}
+	    
+	};
+
 	
 	// Bullet.
 	
